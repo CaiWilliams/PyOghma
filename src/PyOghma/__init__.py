7d6f37cf330a8a9b5cc3a088556a0d5659c89651
cai <cai.williams@physik.tu-chmenitz.de>
2025-05-14 11:14:34 +0200

    ud

--- src/PyOghma/__init__.py
+++ src/PyOghma/__init__.py
@@ -2,7 +2,6 @@ from .OghmaNano import OghmaNano
 from .OghmaResults import Results
 from .Calculate import Ideality_Factor, Transport_Resistance, Psudo_JV
 
-""" PyOghma is a Python package designed for the simulation and analysis of organic optoelectronic devices."""
 """
 .. include:: ../README.md
 """
